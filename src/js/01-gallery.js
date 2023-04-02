d5a770958984435959a7e4457f7cefea859908a1
dimside <dimside29@gmail.com>
2023-04-03 01:40:36 +0300

    First-task-completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,35 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+const galleryEL = document.querySelector('.gallery');
+
+galleryEL.innerHTML = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join('');
+
+galleryEL.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(evt) {
+  evt.preventDefault();
+
+  if (evt.currentTarget === evt.target) return;
+
+  let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+}
