85dfa77771f0c4566236c22fd9f1f1cb96f8f933
dimside <dimside29@gmail.com>
2023-04-03 03:07:48 +0300

    Second-task-completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (evt) {
+    localStorage.setItem('videoplayer-current-time', evt.seconds);
+  }, 1000)
+);
+
+let videoplayerCurrentTime = localStorage.getItem('videoplayer-current-time');
+console.log(videoplayerCurrentTime);
+
+player.setCurrentTime(videoplayerCurrentTime);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
