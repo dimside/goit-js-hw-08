eddd4a8b9ba337309ec23b3ee734c57c96db5567
dimside <dimside29@gmail.com>
2023-04-04 03:17:17 +0300

    Third-task-completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,59 @@
+import throttle from 'lodash.throttle';
 
+const feedbackFormEL = document.querySelector('.feedback-form');
+const emailEl = document.querySelector("[name = 'email']");
+const messageEl = document.querySelector("[name = 'message']");
+
+// ======================== Input-Form Listener ===========================
+feedbackFormEL.addEventListener('input', throttle(onFormElInput, 500));
+
+function onFormElInput(evt) {
+  try {
+    const {
+      elements: { email, message },
+    } = evt.currentTarget;
+
+    const feedbackObj = {
+      email: email.value,
+      message: message.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackObj));
+  } catch {}
+}
+// ======================================================================
+
+// ================= Page-load Form-input value ====================
+function getLocalStorageInfo() {
+  if (JSON.parse(localStorage.getItem('feedback-form-state'))) {
+    const { email: emailValue, message: messageValue } = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+    emailEl.value = emailValue;
+    messageEl.value = messageValue;
+
+    return (formObject = {
+      email: emailValue,
+      message: messageValue,
+    });
+  } else {
+    return (formObject = false);
+  }
+}
+getLocalStorageInfo();
+// =================================================================
+
+// ================== Submit Listener ====================
+feedbackFormEL.addEventListener('submit', onFormElSubmit);
+
+function onFormElSubmit(evt) {
+  evt.preventDefault();
+
+  getLocalStorageInfo();
+  if (formObject) {
+    console.log(formObject);
+  }
+
+  feedbackFormEL.reset();
+  localStorage.clear();
+}
+// =======================================================
